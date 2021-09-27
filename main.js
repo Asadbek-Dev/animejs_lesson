7144ed943cdff16ed80ec531e2b18d958bf88de0
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-09-27 23:11:23 +0500

    all boxs are animated

--- main.js
+++ main.js
@@ -9,3 +9,39 @@ anime({
       easing:'easeInOutSine'
     }
   });
+anime({
+    targets: 'div.box.blue',
+    translateY: [
+      { value:200, duration:500, delay:1000 },
+      { value:0, duration:800 }
+    ],
+    rotate:{
+      value:'1turn',
+      easing:'easeInOutSine',
+      delay:1000
+    }
+  });
+anime({
+    targets: 'div.box.green',
+    translateY: [
+      { value:200, duration:500, delay:2000 },
+      { value:0, duration:800 }
+    ],
+    rotate:{
+      value:'1turn',
+      easing:'easeInOutSine',
+      delay:2000
+    }
+  });
+anime({
+    targets: 'div.box.yellow',
+    translateY: [
+      { value:200, duration:500, delay:3000 },
+      { value:0, duration:800 }
+    ],
+    rotate:{
+      value:'1turn',
+      easing:'easeInOutSine',
+      delay:3000
+    }
+  });
