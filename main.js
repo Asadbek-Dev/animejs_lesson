f25131bc8d3840dc8445784b5bbf1b1d9b72a5c3
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-09-28 22:49:15 +0500

    added clean code

--- main.js
+++ main.js
@@ -1,47 +1,61 @@
+// anime({
+//     targets: 'div.box.red',
+//     translateY: [
+//       { value:200, duration:500 },
+//       { value:0, duration:800 }
+//     ],
+//     rotate:{
+//       value:'1turn',
+//       easing:'easeInOutSine'
+//     }
+//   });
+// anime({
+//     targets: 'div.box.blue',
+//     translateY: [
+//       { value:200, duration:500, delay:1000 },
+//       { value:0, duration:800 }
+//     ],
+//     rotate:{
+//       value:'1turn',
+//       easing:'easeInOutSine',
+//       delay:1000
+//     }
+//   });
+// anime({
+//     targets: 'div.box.green',
+//     translateY: [
+//       { value:200, duration:500, delay:2000 },
+//       { value:0, duration:800 }
+//     ],
+//     rotate:{
+//       value:'1turn',
+//       easing:'easeInOutSine',
+//       delay:2000
+//     }
+//   });
+// anime({
+//     targets: 'div.box.yellow',
+//     translateY: [
+//       { value:200, duration:500, delay:3000 },
+//       { value:0, duration:800 }
+//     ],
+//     rotate:{
+//       value:'1turn',
+//       easing:'easeInOutSine',
+//       delay:3000
+//     }
+//   });
 anime({
-    targets: 'div.box.red',
+    targets: 'div.box',
     translateY: [
-      { value:200, duration:500 },
-      { value:0, duration:800 }
-    ],
-    rotate:{
-      value:'1turn',
-      easing:'easeInOutSine'
-    }
-  });
-anime({
-    targets: 'div.box.blue',
-    translateY: [
-      { value:200, duration:500, delay:1000 },
+      { value:200, duration:500},
       { value:0, duration:800 }
     ],
     rotate:{
       value:'1turn',
       easing:'easeInOutSine',
-      delay:1000
-    }
-  });
-anime({
-    targets: 'div.box.green',
-    translateY: [
-      { value:200, duration:500, delay:2000 },
-      { value:0, duration:800 }
-    ],
-    rotate:{
-      value:'1turn',
-      easing:'easeInOutSine',
-      delay:2000
-    }
-  });
-anime({
-    targets: 'div.box.yellow',
-    translateY: [
-      { value:200, duration:500, delay:3000 },
-      { value:0, duration:800 }
-    ],
-    rotate:{
-      value:'1turn',
-      easing:'easeInOutSine',
-      delay:3000
+    },
+    delay:function(el,i) {
+      return i*1000
     }
   });
